5b59b993b88ad6fd99079116e6848c72aff18050
T.Wada <28886385+TakeshiWada1980@users.noreply.github.com>
2024-12-31 09:47:43 +0900

    create 16 files and update 1 file

---
+++ docs/codes/11/code01.tsx
@@ -0,0 +1,313 @@
+"use client";
+import { useState, useEffect } from "react";
+import { useRouter } from "next/navigation";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSpinner } from "@fortawesome/free-solid-svg-icons";
+import { twMerge } from "tailwind-merge";
+import { useAuth } from "@/app/_hooks/useAuth";
+
+// カテゴリをフェッチしたときのレスポンスのデータ型
+type CategoryApiResponse = {
+  id: string;
+  name: string;
+  createdAt: string;
+  updatedAt: string;
+};
+
+// 投稿記事のカテゴリ選択用のデータ型
+type SelectableCategory = {
+  id: string;
+  name: string;
+  isSelect: boolean;
+};
+
+// 投稿記事の新規作成のページ
+const Page: React.FC = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [fetchErrorMsg, setFetchErrorMsg] = useState<string | null>(null);
+
+  const [newTitle, setNewTitle] = useState("");
+  const [newContent, setNewContent] = useState("");
+  const [newCoverImageURL, setNewCoverImageURL] = useState("");
+  const [newCoverImageKey, setNewCoverImageKey] = useState("hoge"); // ◀ 追加
+
+  const router = useRouter();
+  const { token } = useAuth();
+
+  // カテゴリ配列 (State)。取得中と取得失敗時は null、既存カテゴリが0個なら []
+  const [checkableCategories, setCheckableCategories] = useState<
+    SelectableCategory[] | null
+  >(null);
+
+  // コンポーネントがマウントされたとき (初回レンダリングのとき) に1回だけ実行
+  useEffect(() => {
+    // ウェブAPI (/api/categories) からカテゴリの一覧をフェッチする関数の定義
+    const fetchCategories = async () => {
+      try {
+        setIsLoading(true);
+
+        // フェッチ処理の本体
+        const requestUrl = "/api/categories";
+        const res = await fetch(requestUrl, {
+          method: "GET",
+          cache: "no-store",
+        });
+
+        // レスポンスのステータスコードが200以外の場合 (カテゴリのフェッチに失敗した場合)
+        if (!res.ok) {
+          setCheckableCategories(null);
+          throw new Error(`${res.status}: ${res.statusText}`); // -> catch節に移動
+        }
+
+        // レスポンスのボディをJSONとして読み取りカテゴリ配列 (State) にセット
+        const apiResBody = (await res.json()) as CategoryApiResponse[];
+        setCheckableCategories(
+          apiResBody.map((body) => ({
+            id: body.id,
+            name: body.name,
+            isSelect: false,
+          }))
+        );
+      } catch (error) {
+        const errorMsg =
+          error instanceof Error
+            ? `カテゴリの一覧のフェッチに失敗しました: ${error.message}`
+            : `予期せぬエラーが発生しました ${error}`;
+        console.error(errorMsg);
+        setFetchErrorMsg(errorMsg);
+      } finally {
+        // 成功した場合も失敗した場合もローディング状態を解除
+        setIsLoading(false);
+      }
+    };
+
+    fetchCategories();
+  }, []);
+
+  // チェックボックスの状態 (State) を更新する関数
+  const switchCategoryState = (categoryId: string) => {
+    if (!checkableCategories) return;
+
+    setCheckableCategories(
+      checkableCategories.map((category) =>
+        category.id === categoryId
+          ? { ...category, isSelect: !category.isSelect }
+          : category
+      )
+    );
+  };
+
+  const updateNewTitle = (e: React.ChangeEvent<HTMLInputElement>) => {
+    // ここにタイトルのバリデーション処理を追加する
+    setNewTitle(e.target.value);
+  };
+
+  const updateNewContent = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    // ここに本文のバリデーション処理を追加する
+    setNewContent(e.target.value);
+  };
+
+  const updateNewCoverImageURL = (e: React.ChangeEvent<HTMLInputElement>) => {
+    // ここにカバーイメージURLのバリデーション処理を追加する
+    setNewCoverImageURL(e.target.value);
+  };
+
+  //
+  const updateNewCoverImage = (e: React.ChangeEvent<HTMLInputElement>) => {};
+
+  // フォームの送信処理
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    // ▼ 追加: トークンが取得できない場合はアラートを表示して処理中断
+    if (!token) {
+      window.alert("予期せぬ動作：トークンが取得できません。");
+      return;
+    }
+
+    setIsSubmitting(true);
+
+    try {
+      const requestBody = {
+        title: newTitle,
+        content: newContent,
+        coverImageURL: newCoverImageURL,
+        categoryIds: checkableCategories
+          ? checkableCategories.filter((c) => c.isSelect).map((c) => c.id)
+          : [],
+      };
+      const requestUrl = "/api/admin/posts";
+      console.log(`${requestUrl} => ${JSON.stringify(requestBody, null, 2)}`);
+      const res = await fetch(requestUrl, {
+        method: "POST",
+        cache: "no-store",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: token, // ◀ 追加
+        },
+        body: JSON.stringify(requestBody),
+      });
+
+      if (!res.ok) {
+        throw new Error(`${res.status}: ${res.statusText}`); // -> catch節に移動
+      }
+
+      const postResponse = await res.json();
+      setIsSubmitting(false);
+      router.push(`/posts/${postResponse.id}`); // 投稿記事の詳細ページに移動
+    } catch (error) {
+      const errorMsg =
+        error instanceof Error
+          ? `投稿記事のPOSTリクエストに失敗しました\n${error.message}`
+          : `予期せぬエラーが発生しました\n${error}`;
+      console.error(errorMsg);
+      window.alert(errorMsg);
+      setIsSubmitting(false);
+    }
+  };
+
+  if (isLoading) {
+    return (
+      <div className="text-gray-500">
+        <FontAwesomeIcon icon={faSpinner} className="mr-1 animate-spin" />
+        Loading...
+      </div>
+    );
+  }
+
+  if (!checkableCategories) {
+    return <div className="text-red-500">{fetchErrorMsg}</div>;
+  }
+
+  return (
+    <main>
+      <div className="mb-4 text-2xl font-bold">投稿記事の新規作成</div>
+
+      {isSubmitting && (
+        <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/50">
+          <div className="flex items-center rounded-lg bg-white px-8 py-4 shadow-lg">
+            <FontAwesomeIcon
+              icon={faSpinner}
+              className="mr-2 animate-spin text-gray-500"
+            />
+            <div className="flex items-center text-gray-500">処理中...</div>
+          </div>
+        </div>
+      )}
+
+      <form
+        onSubmit={handleSubmit}
+        className={twMerge("space-y-4", isSubmitting && "opacity-50")}
+      >
+        <div className="space-y-1">
+          <label htmlFor="title" className="block font-bold">
+            タイトル
+          </label>
+          <input
+            type="text"
+            id="title"
+            name="title"
+            className="w-full rounded-md border-2 px-2 py-1"
+            value={newTitle}
+            onChange={updateNewTitle}
+            placeholder="タイトルを記入してください"
+            required
+          />
+        </div>
+
+        <div className="space-y-1">
+          <label htmlFor="content" className="block font-bold">
+            本文
+          </label>
+          <textarea
+            id="content"
+            name="content"
+            className="h-48 w-full rounded-md border-2 px-2 py-1"
+            value={newContent}
+            onChange={updateNewContent}
+            placeholder="本文を記入してください"
+            required
+          />
+        </div>
+
+        <div className="space-y-1">
+          <label htmlFor="coverImageURL" className="block font-bold">
+            カバーイメージ (URL)
+          </label>
+          <input
+            type="url"
+            id="coverImageURL"
+            name="coverImageURL"
+            className="w-full rounded-md border-2 px-2 py-1"
+            value={newCoverImageURL}
+            onChange={updateNewCoverImageURL}
+            placeholder="カバーイメージのURLを記入してください"
+            required
+          />
+        </div>
+
+        <div className="space-y-1">
+          <label htmlFor="coverImageKey" className="block font-bold">
+            カバーイメージ (Key)
+          </label>
+          <input
+            type="url"
+            id="coverImageKey"
+            name="coverImageKey"
+            className="w-full rounded-md border-2 px-2 py-1 text-gray-500"
+            value={newCoverImageKey}
+            disabled
+            readOnly
+            required
+          />
+        </div>
+
+        <input
+          type="file"
+          id="coverImage"
+          onChange={updateNewCoverImage}
+          accept="image/*"
+        />
+
+        <div className="space-y-1">
+          <div className="font-bold">タグ</div>
+          <div className="flex flex-wrap gap-x-3.5">
+            {checkableCategories.length > 0 ? (
+              checkableCategories.map((c) => (
+                <label key={c.id} className="flex space-x-1">
+                  <input
+                    id={c.id}
+                    type="checkbox"
+                    checked={c.isSelect}
+                    className="mt-0.5 cursor-pointer"
+                    onChange={() => switchCategoryState(c.id)}
+                  />
+                  <span className="cursor-pointer">{c.name}</span>
+                </label>
+              ))
+            ) : (
+              <div>選択可能なカテゴリが存在しません。</div>
+            )}
+          </div>
+        </div>
+
+        <div className="flex justify-end">
+          <button
+            type="submit"
+            className={twMerge(
+              "rounded-md px-5 py-1 font-bold",
+              "bg-indigo-500 text-white hover:bg-indigo-600",
+              "disabled:cursor-not-allowed"
+            )}
+            disabled={isSubmitting}
+          >
+            記事を投稿
+          </button>
+        </div>
+      </form>
+    </main>
+  );
+};
+
+export default Page;
